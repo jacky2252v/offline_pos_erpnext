8a23c3ba029a0c4a447f3d0f96571dc6d12fd5dd
jacky2252v <vivekbaraiya000@gmail.com>
2025-02-20 16:14:35 +0530

    Add setup.py for offline POS ERPNext package configuration

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="offline_pos_erpnext",
+    version="0.0.1",
+    description="Offline POS for ERPNext",
+    author="Your Name",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[]
+)
